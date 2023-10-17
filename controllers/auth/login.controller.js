421a2f36a49b265654cf415dd52e1971111c7ad9
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-10-17 21:39:55 +0330

    feat: users can now login and fetch their jwt token from the response

--- controllers/auth/login.controller.js
+++ controllers/auth/login.controller.js
@@ -1,6 +1,24 @@
-'use strict';
-const loginController = (req, res) => {
-	res.send('logging...');
+const jwt = require('jsonwebtoken');
+
+('use strict');
+
+const userModel = require('../../models/user.model');
+const { verifyPassword, genToken } = require('../../utils/auth.utils');
+
+const loginController = async (req, res) => {
+	let { email, password } = req.body;
+	let user = await userModel.findOne({ email });
+	if (!user || !verifyPassword(password, user.password))
+		return res.status(404).send({
+			code: res.statusCode,
+			error: { message: 'email or password is invalid' },
+		});
+	let token = genToken({ email: user.email, password: user.password });
+	res.status(200).json({
+		code: res.statusCode,
+		message: 'successfully logged in 😁',
+		token,
+	});
 };
 
 module.exports = loginController;
--- utils/auth.utils.js
+++ utils/auth.utils.js
@@ -1,5 +1,9 @@
 'use strict';
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const sha1 = require('sha1');
+
+const JWT_SECRET = sha1('my JWT auth token secret');
 
 const hashPassword = (password) => {
 	let salt = bcrypt.genSaltSync(10);
@@ -7,4 +11,12 @@ const hashPassword = (password) => {
 	return hash;
 };
 
-module.exports = hashPassword;
+const verifyPassword = (password, hash) => {
+	return bcrypt.compareSync(password, hash);
+};
+
+const genToken = (payload, secret = JWT_SECRET) => {
+	return jwt.sign(payload, secret);
+};
+
+module.exports = { hashPassword, verifyPassword, genToken };
