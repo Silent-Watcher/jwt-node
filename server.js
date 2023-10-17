0b8c02413f7ca4a3fdefe3c0fb573776ea54a215
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-10-17 20:11:11 +0330

    feat:add error handler middlewares

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const http = require('http');
+const errorHandler = require('./middlewares/global/errorHandlerMiddleware');
+const notFoundErrorHandler = require('./middlewares/global/notFoundErrorHandler.middleware');
 const app = express();
 const PORT = 3000;
 
 const server = http.createServer({}, app);
 
+app.use(express.json(), express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('hello from simple server :)');
 });
+
+app.use(notFoundErrorHandler);
+app.use(errorHandler);
 
 server.listen(PORT, () =>
   console.log(`server is running on port : ${PORT},\n http://localhost:${PORT}`)
